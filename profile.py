c6893c9b81ffae3bbcf9106d23c412be8e7b1be8
bchay <b.chay9@gmail.com>
2020-04-27 20:41:19 -0500

    Fixed issue with dividing by zero when computing average

--- profile.py
+++ profile.py
@@ -13,6 +13,9 @@ def profile():
     print(user_reviews)
 
     rating_num = len(user_reviews)
-    rating_average = round(sum([i[0] for i in user_reviews]) / rating_num, 1)
+    rating_average = 0
+
+    if rating_num > 0:
+        rating_average = round(sum([i[0] for i in user_reviews]) / rating_num, 1)
 
     return render_template("profile.html", user_info=user_info, user_reviews=user_reviews, rating_num=rating_num, rating_average=rating_average)
