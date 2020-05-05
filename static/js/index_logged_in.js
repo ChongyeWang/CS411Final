4ac822598596612c644761ef0348aab75242b812
bchay <b.chay9@gmail.com>
2020-05-05 16:31:16 -0500

    Fixed CSS issues with homepage

--- static/js/index_logged_in.js
+++ static/js/index_logged_in.js
@@ -10,6 +10,7 @@ function setMovieList() {
         labelField: "title",
         valueField: "title",
         searchField: "title",
+        placeholder: "Search for a movie",
         onChange: (movie) => {
             window.location = "/movie?movie_name=" + encodeURIComponent(movie);
         },
